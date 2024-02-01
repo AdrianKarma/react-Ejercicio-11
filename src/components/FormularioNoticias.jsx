5d3e96ac9eec27ab84b1322fb148c6ca9209740d
Adrian Karma <adrian.karma.chan@gmail.com>
2024-02-01 04:37:09 -0300

    Actualizar FormularioNoticias.jsx

--- src/components/FormularioNoticias.jsx
+++ src/components/FormularioNoticias.jsx
@@ -5,7 +5,7 @@ import ListaNoticias from './ListaNoticias';
 
 
 const FormularioNoticias = () => {
-    const apiKey = "pub_363241281d1aac4e63ea34d1f2c239bbb99df"
+    const apiKey = "pub_374711e79b54a5d8e28ce262a2ae770ff0fea"
     const [noticias, setNoticias] =useState ([])
     const [categoria, setCategoria] = useState("food");
 
@@ -16,9 +16,7 @@ useEffect(()=>{
     
     const consultarAPI = async()=>{
     const respuesta = await fetch(`https://newsdata.io/api/1/news?apikey=${apiKey}&language=es&category=${categoria} `)
-    //pub_363241281d1aac4e63ea34d1f2c239bbb99df
-    //pub_374711e79b54a5d8e28ce262a2ae770ff0fea
-    //pub_37483ff3d4d2ec0c099a81f2f3715e4f4d1b0
+    
     const datos = await respuesta.json()
     console.log(datos)
     setNoticias(datos.results)
